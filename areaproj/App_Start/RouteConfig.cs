b7e89478d193639334326a1c5004f578fbe2b6b0
Jason446620 <panshubei@outlook.com>
2020-07-06 14:48:09 +0800

    Add project files.

---
+++ areaproj/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace areaproj
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            var route = routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "areaproj.Areas.Admin.Controllers" }
+            );
+            route.DataTokens["area"] = "Admin";
+        }
+    }
+}
---
+++ areaproj/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace areaproj.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            bool IsAuth = false;
+            if (IsAuth)
+                return View();
+            else
+                return View("~/Areas/Admin/Views/Home/test.cshtml");
+            
+        }
+        public ActionResult test()
+        {
+           return View();
+        }
+    }
+}
